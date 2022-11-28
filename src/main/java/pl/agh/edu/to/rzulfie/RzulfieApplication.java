815cb74441936555daa57a9756fbf0487e4561c9
Mateusz Wejman <407077@student.agh.edu.pl>
2022-11-28 16:17:26 +0100

    Initial commit

---
+++ src/main/java/pl/agh/edu/to/rzulfie/RzulfieApplication.java
@@ -0,0 +1,13 @@
+package pl.agh.edu.to.rzulfie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RzulfieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RzulfieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/agh/edu/to/rzulfie/RzulfieApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.agh.edu.to.rzulfie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RzulfieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
