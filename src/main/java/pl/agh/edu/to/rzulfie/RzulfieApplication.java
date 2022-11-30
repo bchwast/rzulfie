a6b89068130a829c101abe9d2da10dec81d9cf49
Piotr Kowalczyk <piotr.kowalczyk@codewise.com>
2022-11-30 11:05:55 +0100

    Make javafx work with spring

--- src/main/java/pl/agh/edu/to/rzulfie/RzulfieApplication.java
+++ src/main/java/pl/agh/edu/to/rzulfie/RzulfieApplication.java
@@ -1,13 +1,29 @@
 package pl.agh.edu.to.rzulfie;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RzulfieApplication {
+public class RzulfieApplication extends Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(RzulfieApplication.class, args);
+        Application.launch();
     }
 
+    @Override
+    public void init() {
+        SpringApplication.run(getClass()).getAutowireCapableBeanFactory().autowireBean(this);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pane helloPane = new Pane(new Label("Hello JavaFx"));
+        primaryStage.setScene(new Scene(helloPane));
+        primaryStage.show();
+    }
 }
