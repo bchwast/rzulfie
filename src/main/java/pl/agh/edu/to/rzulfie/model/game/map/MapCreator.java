143f010e8795ae7d96a8b42c54862cb20df67170
Piotr Witek <72527323+Viciooo@users.noreply.github.com>
2022-12-14 01:10:58 +0100

    added manifold + init of MapCreator

--- src/main/java/pl/agh/edu/to/rzulfie/model/game/map/MapCreator.java
+++ src/main/java/pl/agh/edu/to/rzulfie/model/game/map/MapCreator.java
@@ -1,4 +1,60 @@
 package pl.agh.edu.to.rzulfie.model.game.map;
 
+import pl.agh.edu.to.rzulfie.model.game.utils.Vector;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapCreator {
+    private Map<Vector, MapField> fieldsByPosition = new HashMap<>();
+    private final Vector mapSize;
+    private Vector startPosition = null;
+    private Vector finishPosition = null;
+
+    public MapCreator(Vector mapSize) {
+        this.mapSize = mapSize;
+    }
+
+    public GridMap create() {
+        return new GridMap(
+                fieldsByPosition,
+                mapSize,
+                finishPosition,
+                startPosition
+        );
+    }
+
+    public void addMapField(Vector position) {
+        MapField mapField = new MapField(Collections.emptyList(),position);
+        fieldsByPosition.put(position,mapField);
+    }
+
+    public void connectMapFields(Vector positionA, Vector positionB) {
+
+    }
+
+    public void markAsStartField(Vector position) {
+        if(fieldsByPosition.containsKey(position)){
+            this.startPosition = position;
+        }
+    }
+
+    public void markAsFinishField(Vector position) {
+        if(fieldsByPosition.containsKey(position)){
+            this.finishPosition = position;
+        }
+    }
+
+    public Map<Vector, MapField> getFieldsByPosition() {
+        return fieldsByPosition;
+    }
+
+    public Vector getStartPosition() {
+        return startPosition;
+    }
+
+    public Vector getFinishPosition() {
+        return finishPosition;
+    }
 }
